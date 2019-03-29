6137a4dbda56e7f7cad82be2077bbb211acbe75a
Mark <mark@laptop-osacu7vr>
2019-03-29 17:08:16 +0100

    removing wrapper

--- slotex-nlp-app/src/main/java/si/slotex/web/slotexnlpapp/SlotexNlpAppApplication.java
+++ slotex-nlp-app/src/main/java/si/slotex/web/slotexnlpapp/SlotexNlpAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SlotexNlpAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SlotexNlpAppApplication.class, args);
 	}
 	
