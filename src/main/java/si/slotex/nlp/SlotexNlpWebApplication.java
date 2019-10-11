3edceee832da970cbad0d39bd07aff88d34f027b
tadejj <tadej.justin@medius.si>
2019-10-11 11:37:04 +0200

    Disable mongo autoconfiguration

--- src/main/java/si/slotex/nlp/SlotexNlpWebApplication.java
+++ src/main/java/si/slotex/nlp/SlotexNlpWebApplication.java
@@ -3,9 +3,15 @@ package si.slotex.nlp;
 import com.vaadin.flow.spring.annotation.EnableVaadin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+
 
 @EnableVaadin("si.slotex.nlp")
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class,MongoDataAutoConfiguration.class})
 public class SlotexNlpWebApplication {
 
 	public static void main(String[] args) {
