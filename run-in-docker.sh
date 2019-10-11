14d62c9c9fb1eb9d66a624e8cabea81904463396
tadejj <tadej.justin@medius.si>
2019-10-11 22:51:50 +0200

    Correcting run-in-docker.sh and using it in travis

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -71,7 +71,7 @@ check_if_var_is_set SLOTEX_NLP_CORE_TAG
 check_if_var_is_set SLOTEX_NLP_WEB_TAG
 
 # parse args and switches
-options=$(getopt -o hrpdce: --long help,remove,pull,down,check,env: -n 'parse-options' -- "$@")
+options=$(getopt -o hrpdct --long help,remove,pull,down,check,ittest -n 'parse-options' -- "$@")
 if [ "$?" != "0" ]; then  
     echo "ERROR options provided"
     usage;
@@ -92,20 +92,18 @@ while true; do
     -d | --down ) down=true; shift; shift ;;
     -c | --check ) check=true; shift; shift ;;
     -r | --remove ) rmi=true; shift; shift ;;
-    -e | --env ) env="$2"; shift; shift ;;
+    -t | --ittest ) env=true; shift; shift ;;
     -- ) shift; break;;
     *) usage; break;;
     esac
 done
 
-if [ "$env" = false ]; then
-    echo "Please set --env argument"
-    usage
-    exit 2
+if [ "$env" = "true" ]; then
+    echo "Running $0 with --ittest switch. This will only prepare environment for integration testing."
 fi
 
-if $pull; then
-    if [ "$env" != "it-test" ]; then
+if [ "$pull" = "true" ]; then
+    if [ "$env" = "false" ]; then
         docker-compose pull -q
     else
         docker-compose pull -q redis mongodb slotex-nlp-core
@@ -113,17 +111,13 @@ if $pull; then
     run=false
 fi
 
-if $down; then
-    if [ "$env" != "it-test" ]; then
-        docker-compose down -v
-    else
-        docker-compose down -v redis mongodb slotex-nlp-core
-    fi
+if [ "$down" = "true" ]; then
+    docker-compose down -v
     run=false
 fi
 
-if $rmi; then
-    if [ "$env" != "it-test" ]; then
+if [ "$rmi" = "true" ]; then
+    if [ "$env" = "false" ]; then
         docker-compose down -v --rmi all
     else
         docker-compose down -v --rmi redis mongodb slotex-nlp-core
@@ -131,13 +125,13 @@ if $rmi; then
     run=false
 fi
 
-if $check; then
+if [ "$check" = "true" ]; then
     docker-compose -f docker-compose.yml config -q
     run=false
 fi
 
-if $run; then
-    if [ "$env" != "it-test" ]; then
+if [ "$run" = "true" ]; then
+    if [ "$env" = "false" ]; then
         docker-compose up -d
     else
         docker-compose up -d redis mongodb slotex-nlp-core
